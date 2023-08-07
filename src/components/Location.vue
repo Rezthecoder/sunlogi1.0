af1272c5429290b76c712507810afc7501f489c9
kosaka <kosaka_k@sunseer.co.jp>
2023-08-07 11:43:35 +0900

    location ,login

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,21 +1,29 @@
 <script>
 import Footer from "../components/Footer.vue";
+import MyHeader from "../components/MyHeader.vue";
 export default {
   components: {
     Footer, // Register the Footer component
+    MyHeader,
   },
 };
 </script>
 
 <template>
+  
   <div class="container ">
+   
     <div class="row">
+      
       <div class="col-sm-12 col-md-6 offset-md-3">
         <div class="card my-10 custom-card">
+          <MyHeader msg="ロケーション選択"/>
           <div class="card-body p-lg-5">
+          
             <div class="text-center">
               <!-- Removed the inner row and adjusted column classes -->
               <div class="form-group row mb-3">
+              
                 <label class="col-sm-5 col-form-label" for="location">ロケーション</label>
                 <div class="col-sm-6">
                   <input type="text" class="form-control" id="location" required="true" />
