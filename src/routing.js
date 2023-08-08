8036ce687fc89bc6ecdb824aed852fa97b25e3e2
suresh <lama_s@sunseer.co.jp>
2023-08-08 14:40:10 +0900

    routing modified

--- src/routing.js
+++ src/routing.js
@@ -7,13 +7,9 @@ import Syohin from "./components/Syohin.vue"
 import SyohinInfo from "./components/SyohinInfo.vue"
 import LocationKanryo from "./components/LocationKanryo.vue"
 import Menu from "./components/Menu.vue";
-<<<<<<< HEAD
 import Nyuuko from "./components/Nyuuko.vue";
-
-=======
 import Message from "./components/Message.vue";
 import SyohinKanryo from "./components/SyohinKanryo.vue";
->>>>>>> 01c8813799ae987306db29779f382fa2f71c8b97
 //login routes
 const routes =[
     {
