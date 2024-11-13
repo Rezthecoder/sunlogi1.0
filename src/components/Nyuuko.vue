1f487181daacf70b5fee5d16f030ef4eb4dbe618
rez <noobprogramming111@gmail.com>
2024-11-13 21:55:26 +0900

    fixed

--- src/components/Nyuuko.vue
+++ src/components/Nyuuko.vue
@@ -1,41 +1,38 @@
 <script>
 import Footer from "./Footer.vue";
 import MyHeader from "./MyHeader.vue";
-import BarcodeForm from "./BarcodeForm.vue";
+// import BarcodeForm from "./BarcodeForm.vue";
 import Form from "./Form.vue";
 export default {
   components: {
     Footer, // Register the Footer component
     MyHeader,
-    BarcodeForm,
+    // BarcodeForm,
     Form,
   },
 };
-
 </script>
 
 <template>
-  
-  <div class="container ">
-   
+  <div class="container">
     <div class="row">
-      
       <div class="col-sm-12 col-md-6 offset-md-3">
         <div class="card my-10 custom-card">
-          <MyHeader msg="入庫新規登録"/>
+          <MyHeader msg="入庫新規登録" />
           <div class="card-body p-lg-5">
-          
             <div class="text-center">
               <!-- Removed the inner row and adjusted column classes -->
               <div class="form-group row mb-3">
-          
-               <Form class="form"/>
-
-
+                <Form class="form" />
               </div>
             </div>
           </div>
-          <Footer msg1="戻る" msg2="完了" :msg3="'/menu'" :msg4="'/tourokuKanryou'"></Footer>
+          <Footer
+            msg1="戻る"
+            msg2="完了"
+            :msg3="'/menu'"
+            :msg4="'/tourokuKanryou'"
+          ></Footer>
         </div>
       </div>
     </div>
@@ -45,30 +42,25 @@ export default {
 
 <style>
 /* Add your custom CSS styles for the dropdown here */
- /* .custom {
+/* .custom {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 300px;
 }  */
 
-
-@media (width:240px) and (height:320px) {
-  
-  .form{
-    height:73vh;
+@media (width: 240px) and (height: 320px) {
+  .form {
+    height: 73vh;
   }
-  .form{
-    height:fit-content;
+  .form {
+    height: fit-content;
   }
-  .input-group-text{
-    font-size:11px;
+  .input-group-text {
+    font-size: 11px;
   }
-  .form-control{
-    font-size:11px;
+  .form-control {
+    font-size: 11px;
   }
 }
-
-
-
 </style>
